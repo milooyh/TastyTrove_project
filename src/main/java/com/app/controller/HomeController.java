4967a1305f37eee0134a3ae88cdbf6920c226e5d
donghun723 <donghun723@naver.com>
2024-02-14 09:45:11 +0900

    admin 경로및 상품 html 수정

--- src/main/java/com/app/controller/HomeController.java
+++ src/main/java/com/app/controller/HomeController.java
@@ -9,7 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.app.dto.postRecipe.PostRecipe;
 import com.app.dto.postRecipe.RecipeSearchCondition;
+import com.app.dto.product.Product;
+import com.app.dto.product.ProductSearchCondition;
 import com.app.service.postRecipe.PostRecipeService;
+import com.app.service.product.ProductService;
 
 
 @Controller
@@ -19,10 +22,14 @@ public class HomeController {
 	@Autowired
 	PostRecipeService postRecipeService;
 
-	
+	@Autowired
+	ProductService productSerive;
 	
 	@RequestMapping("/")
-	public String home(Model model, RecipeSearchCondition recipeSearchCondition) {
+	public String home(Model model, RecipeSearchCondition recipeSearchCondition,
+			ProductSearchCondition productSearchCondition
+			
+			) {
 
 //return "/WEB-INF/views/home.jsp";
 //return "/WEB-INF/views/mainpage.jsp";
@@ -31,10 +38,12 @@ public class HomeController {
 		
 //		테스트용(MJ)
 		List<PostRecipe> recipeList = postRecipeService.findRecipeListBySearchCondition(recipeSearchCondition);
-		
+		 List<Product> productList = productSerive.findProductListBySearchCondition(productSearchCondition);
 		model.addAttribute("recipeList", recipeList);
-		
+		 model.addAttribute("productList", productList);
 		return "main";
+		
+		
 		
 	}
 	
